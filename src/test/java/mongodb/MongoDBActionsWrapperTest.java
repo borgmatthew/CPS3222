d7c4156c6183ff8cb6f89c6b6e23e43d453a527b
borgmatthew <borgmb@gmail.com>
2014-11-02 16:46:47 +0100

    Created mongo db action test

---
+++ src/test/java/mongodb/MongoDBActionsWrapperTest.java
@@ -0,0 +1,81 @@
+package mongodb;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.WriteResult;
+
+public class MongoDBActionsWrapperTest {
+
+	MongoDBAtionsWrapper database;
+
+	@Mock
+	MongoClient client;
+
+	@Mock
+	DB db;
+
+	@Mock
+	DBCollection collection;
+
+	@Mock
+	DBObject object;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		database = new MongoDBActionsWrapperImpl(client);
+	}
+
+	@Test
+	public void testInsertSuccessfull() {
+		// given
+		WriteResult writeResult = mock(WriteResult.class);
+		// when
+		doReturn(db).when(client).getDB(anyString());
+		doReturn(collection).when(db).getCollection(anyString());
+		doReturn(writeResult).when(collection).insert(object);
+		doReturn(1).when(writeResult).getN();
+		// then
+		assertTrue(database.insert("databaseName", "tableName", object));
+	}
+
+	@Test
+	public void testInsertMongoExceptionOccurs() {
+		// given
+		DBObject object = mock(DBObject.class);
+		// when
+		doReturn(db).when(client).getDB(anyString());
+		doReturn(collection).when(db).getCollection(anyString());
+		doThrow(new MongoException("test")).when(collection).insert(object);
+		// then
+		assertFalse(database.insert("databaseName", "tableName", object));
+	}
+
+	@Test
+	public void testInsertNotAllElementsInserted() {
+		// given
+		WriteResult writeResult = mock(WriteResult.class);
+		// when
+		doReturn(db).when(client).getDB(anyString());
+		doReturn(collection).when(db).getCollection(anyString());
+		doReturn(writeResult).when(collection).insert(object);
+		doReturn(0).when(writeResult).getN();
+		// then
+		assertFalse(database.insert("databaseName", "tableName", object));
+	}
+}
