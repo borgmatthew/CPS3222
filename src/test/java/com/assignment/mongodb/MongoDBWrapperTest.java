a2305d7b04f299066e9fd43262662bd69c509a31
borgmatthew <borgmb@gmail.com>
2014-11-05 12:42:01 +0100

    Created test to check the find operation when the connection is null

--- src/test/java/com/assignment/mongodb/MongoDBWrapperTest.java
+++ src/test/java/com/assignment/mongodb/MongoDBWrapperTest.java
@@ -1,6 +1,7 @@
 package com.assignment.mongodb;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
@@ -11,11 +12,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
@@ -82,5 +87,21 @@ public class MongoDBWrapperTest {
 		mongoDBwrapper.insert("", "", object);
 		// then
 		verify(testspy).disconnect(mockClient);
+	}
+	
+	@Test
+	public void testFindConnectionNotNull() {
+		// given
+		MongoClient mockClient = mock(MongoClient.class);
+		DBCursor mockCursor = mock(DBCursor.class);
+		List<DBObject> result = new ArrayList<DBObject>();
+		result.add(object);
+		// when
+		doReturn(result).when(mockCursor).toArray();
+		doReturn(mockClient).when(connectionWrapper).connect(anyString(),
+				anyInt());
+		doReturn(mockCursor).when(actionsWrapper).find(any(MongoClient.class), anyString(),	anyString(), any(DBObject.class));
+		// then
+		assertNotNull(mongoDBwrapper.find("", "", object));
 	}
 }
