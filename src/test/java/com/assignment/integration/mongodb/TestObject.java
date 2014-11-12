8e7c8af9b0c944246f60c6ae5956e64dde25384a
borgmatthew <borgmb@gmail.com>
2014-11-13 00:19:44 +0100

    Created TestObject class to test with in integration tests

---
+++ src/test/java/com/assignment/integration/mongodb/TestObject.java
@@ -0,0 +1,39 @@
+package com.assignment.integration.mongodb;
+
+import com.mongodb.BasicDBObject;
+
+public class TestObject extends BasicDBObject {
+
+	private static final long serialVersionUID = 1L;
+
+	int var1 = 0;
+	String var2 = "";
+	
+	public TestObject(){	
+	}
+	
+	public TestObject(int var1, String var2){
+		this.var1 = var1;
+		this.var2 = var2;
+		this.put("var1", var1);
+		this.put("var2", var2);
+	}
+	
+	public int getVar1() {
+		return var1;
+	}
+
+	public void setVar1(int var1) {
+		this.var1 = var1;
+		this.put("var1", var1);
+	}
+
+	public String getVar2() {
+		return var2;
+	}
+
+	public void setVar2(String var2) {
+		this.var2 = var2;
+		this.put("var2", var2);
+	}
+}
