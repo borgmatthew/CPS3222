f2b5c015adc1be683bee51768d909a562fe408ee
borgmatthew <borgmb@gmail.com>
2014-11-13 00:18:50 +0100

    Created methods to insert and find bets from database

--- src/main/java/com/assignment/DBObjects/Bets.java
+++ src/main/java/com/assignment/DBObjects/Bets.java
@@ -1,16 +1,40 @@
 package com.assignment.DBObjects;
 
-public class Bets {
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+public class Bets extends BasicDBObject{
 	
+	private static final long serialVersionUID = 1L;
 	private int betId;
 	private String risk;
-	private String ammount;
+	private String amount;
 	private int userId;
+	
+	public Bets(){
+	}
+	
+	public Bets(DBObject fromObject){
+		this.amount = (String) fromObject.get("amount");
+		this.betId = (Integer) fromObject.get("betId");
+		this.risk = (String) fromObject.get("risk");
+		this.userId = (Integer) fromObject.get("userId");
+		populateMap();
+	}
 	
 	public Bets(int betId, int userId, String risk, String ammount) {
 		this.betId = betId;
 		this.risk = risk;
-		this.ammount = ammount;
+		this.amount = ammount;
+		this.userId = userId;
+		populateMap();
+	}
+	
+	public void populateMap(){
+		this.put("betId", betId);
+		this.put("risk", risk);
+		this.put("amount", amount);
+		this.put("userId", userId);
 	}
 
 	public int getBetId() {
@@ -19,6 +43,7 @@ public class Bets {
 
 	public void setBetId(int betId) {
 		this.betId = betId;
+		this.put("betId", betId);
 	}
 
 	public String getRisk() {
@@ -27,18 +52,21 @@ public class Bets {
 
 	public void setRisk(String risk) {
 		this.risk = risk;
+		this.put("risk", risk);
 	}
 
 	public String getAmmount() {
-		return ammount;
+		return amount;
 	}
 
-	public void setAmmount(String ammount) {
-		this.ammount = ammount;
+	public void setAmmount(String amount) {
+		this.amount = amount;
+		this.put("amount", amount);
 	}
 	
 	public void setUserId(int userId){
 		this.userId = userId;
+		this.put("userId", userId);
 	}
 	
 	public int getUserId(){
--- src/main/java/com/assignment/requests/BettingRequestsImpl.java
+++ src/main/java/com/assignment/requests/BettingRequestsImpl.java
@@ -1,21 +1,34 @@
 package com.assignment.requests;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.assignment.DBObjects.Bets;
+import com.assignment.mongodb.MongoDBWrapper;
+import com.assignment.mongodb.MongoDBWrapperImpl;
+import com.mongodb.DBObject;
 
 public class BettingRequestsImpl implements BettingRequests {
 
+	MongoDBWrapper dbWrapper;
+	
+	public BettingRequestsImpl() {
+		dbWrapper = new MongoDBWrapperImpl("localhost", 27017);
+		// TODO load properties from properties file
+	}
+	
 	@Override
 	public boolean createBet(Bets bet) {
-		// TODO Auto-generated method stub
-		return false;
+		return dbWrapper.insert("SoftwareTesting", "Bets", bet);
 	}
 
 	@Override
 	public List<Bets> findBet(Bets toFind) {
-		// TODO Auto-generated method stub
-		return null;
+		List<DBObject> query = dbWrapper.find("SoftwareTesting", "Bets", toFind);
+		List<Bets> result = new ArrayList<Bets>(query.size());
+		for(DBObject o : query){
+			result.add(new Bets(o));
+		}
+		return result;
 	}
-
 }
