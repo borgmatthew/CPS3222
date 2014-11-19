09359f8fcb07731ffb5cdd92446451f1d5629082
borgmatthew <borgmb@gmail.com>
2014-11-19 11:35:23 +0100

    Login validation using password and number of attempts

--- src/main/java/com/assignment/validations/Login.java
+++ src/main/java/com/assignment/validations/Login.java
@@ -1,5 +1,12 @@
 package com.assignment.validations;
 
+import java.util.List;
+
+import com.assignment.DBObjects.User;
+
 public interface Login {
 	public boolean validate(String user,String pass);
+	public boolean userExists(List<User> users);
+	public boolean canLogin(User user);
+	public boolean validatePassword(User user, String password);
 }
--- src/main/java/com/assignment/validations/LoginImp.java
+++ src/main/java/com/assignment/validations/LoginImp.java
@@ -1,26 +1,41 @@
 package com.assignment.validations;
 
+import java.util.List;
+
 import com.assignment.DBObjects.User;
 import com.assignment.requests.UserRequest;
 import com.assignment.requests.UserRequestImpl;
 
 public class LoginImp implements Login{
 
-		public boolean validate(String username,String pass){
-			if(validateUsername(username)==false){
-			return false;
+		public boolean validate(String username,String password){
+			User user = new User();
+			user.setUsername(username);
+			UserRequest request = new UserRequestImpl();
+			List<User> query = request.getUser(user);
+			if(userExists(query)){
+				user = query.get(0);
+				if(canLogin(user)){
+					if(validatePassword(user, password)){
+						return true;
+					}else{
+						user.setAttempts(user.getAttempts() + 1);
+						request.save(user);
+					}
+				}
 			}
-			return true;
+			return false;
 		}
 		
-		public boolean validateUsername(String user){
-			UserRequest request=new UserRequestImpl();
-			User tempUser=new User();
-			tempUser.setUsername(user);
-			if(request.getUser(tempUser).size()>0){
-				return true;
-			}else{
-				return false;
-			}
+		public boolean userExists(List<User> users){
+			return users.size() == 1;
+		}
+		
+		public boolean canLogin(User user){
+			return user.getAttempts() % 3 != 0;
+		}
+		
+		public boolean validatePassword(User user, String password){
+			return user.getPassword().compareTo(password) == 0;
 		}
 }
