1730c465ca09865406fc5708fcd453c27738a736
borgmatthew <borgmb@gmail.com>
2014-11-26 20:26:57 +0100

    Validating both risk and amount

--- src/main/java/com/assignment/validations/BettingValidations.java
+++ src/main/java/com/assignment/validations/BettingValidations.java
@@ -1,5 +1,11 @@
 package com.assignment.validations;
 
-public interface BettingValidations {
+import java.util.List;
 
+import com.assignment.DBObjects.Bet;
+import com.assignment.DBObjects.User;
+
+public interface BettingValidations {
+	public boolean validateRisk(User user, String risk);
+	public boolean validateAmount(User user, double amount, List<Bet> bets);
 }
--- src/main/java/com/assignment/validations/BettingValidationsImp.java
+++ src/main/java/com/assignment/validations/BettingValidationsImp.java
@@ -2,29 +2,48 @@ package com.assignment.validations;
 
 import java.util.List;
 
+import com.assignment.DBObjects.Bet;
 import com.assignment.DBObjects.User;
-import com.assignment.requests.UserRequestImpl;
 
-public class BettingValidationsImp {
-	public boolean validateRisk(String user, String risk) {
-		UserRequestImpl req = new UserRequestImpl();
-		User tempusr = new User();
-		tempusr.setUsername(user);
-		List<User> usr = req.getUser(tempusr);
-		//System.out.println("account type of user is: "+usr.get(0).getAccounttype());
-
-		if ((usr.get(0).getAccounttype()).equals("free")) {
+public class BettingValidationsImp implements BettingValidations{
+	
+	public boolean validateRisk(User user, String risk) {
+		if (isFreeUser(user)) {
 			//System.out.println("hawn dahal");
-			if(risk.equals("High")){
+			//Fejn dahal??
+			if(!risk.equals("Low")){
 				return false;
-			}
-			else if(risk.equals("Medium")){
-				return false;
-			}
-			else{
-				return true;
 			}
 		}
 		return true;
+	}
+	
+	public boolean validateAmount(User user, double amount, List<Bet> bets){
+		if(isFreeUser(user)){
+			return validateAmountFree(bets, amount);
+		}
+		else{
+			return validateAmountPremium(bets, amount);
+		}
+	}
+	
+	private boolean validateAmountFree(List<Bet> bets, double amount){
+		return bets.size() < 3 && amount > 0 && amount <= 5;
+	}
+	
+	private boolean validateAmountPremium(List<Bet> bets, double amount){
+		double total = 0;
+		for(Bet b : bets){
+			total += b.getAmmount();
+		}
+		
+		if(total + amount > 5000){
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean isFreeUser(User user){
+		return user.getAccounttype().equals("free");
 	}
 }
