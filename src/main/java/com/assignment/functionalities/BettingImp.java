a389224ea552235defcb44b47d68089feda40bd7
borgmatthew <borgmb@gmail.com>
2014-11-26 15:17:14 +0100

    Added method to return all bets of user in an html formatted string

--- src/main/java/com/assignment/functionalities/BettingImp.java
+++ src/main/java/com/assignment/functionalities/BettingImp.java
@@ -1,5 +1,7 @@
 package com.assignment.functionalities;
 
+import java.util.List;
+
 import com.assignment.DBObjects.Bet;
 import com.assignment.requests.BettingRequestsImpl;
 import com.assignment.validations.BettingValidationsImp;
@@ -25,9 +27,33 @@ public class BettingImp implements Betting {
 	public String getMessage(){
 		return message;
 	}
+	
 	public void addBet(String risk, String ammount,String user) {
 		BettingRequestsImpl userRequest = new BettingRequestsImpl();
 		Bet newBet =new Bet(risk,ammount,user);
 		userRequest.createBet(newBet);
+	}
+	
+	public String getAllBets(String user){
+		List<Bet> bets = getAllBetsOfUser(user);		
+		StringBuilder builder = new StringBuilder();
+		builder.append("<ul>");
+		for (Bet b : bets) {
+			builder.append("<li>");
+			builder.append(b.getRisk());
+			builder.append(", ");
+			builder.append(b.getAmmount());
+			builder.append("</li>");
+
+		}
+		builder.append("</ul>");
+		return builder.toString();
+	}
+	
+	private List<Bet> getAllBetsOfUser(String user){
+		BettingRequestsImpl betRequest = new BettingRequestsImpl();
+		Bet tempbet = new Bet();
+		tempbet.setUser(user);
+		return betRequest.findBet(tempbet);
 	}
 }
