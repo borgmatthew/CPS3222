f7e47f25e887d2afaabd67216a3f6c699b39f93a
borgmatthew <borgmb@gmail.com>
2014-11-26 20:24:38 +0100

    finished betting validation moved method to get the list of all bets as string in here

--- src/main/java/com/assignment/functionalities/Betting.java
+++ src/main/java/com/assignment/functionalities/Betting.java
@@ -1,5 +1,7 @@
 package com.assignment.functionalities;
 
 public interface Betting {
-
+	public String getAllBets(String user);
+	public boolean validateBets(String user, String risk, double amount);
+	public void addBet(String risk, double amount, String user);
 }
--- src/main/java/com/assignment/functionalities/BettingImp.java
+++ src/main/java/com/assignment/functionalities/BettingImp.java
@@ -3,39 +3,33 @@ package com.assignment.functionalities;
 import java.util.List;
 
 import com.assignment.DBObjects.Bet;
+import com.assignment.DBObjects.User;
 import com.assignment.requests.BettingRequestsImpl;
+import com.assignment.requests.UserRequestImpl;
 import com.assignment.validations.BettingValidationsImp;
 
 public class BettingImp implements Betting {
-    private String message="";   
-	public boolean validateBets(String user,String risk,String ammount){
-		BettingValidationsImp validation=new BettingValidationsImp();
-    	   if(validation.validateRisk(user,risk)){
-   			/*System.out.println("fucker1");
-			System.out.println(user);
-			System.out.println(risk);*/
 
-
-    		   return true;
-    	   }
-    	   else{
-    		   message="A free user can only make low risk bets.";
-    		   return false;
-    	   }
-       }
-	
-	public String getMessage(){
-		return message;
+	public boolean validateBets(String user, String risk, double amount) {
+		BettingValidationsImp validation = new BettingValidationsImp();
+		User usr = getUser(user).get(0);
+		boolean result = false;
+		if (validation.validateRisk(usr, risk)) {
+			if(validation.validateAmount(usr, amount, getAllBetsOfUser(user))){
+				result = true;
+			}
+		}
+		return result;
 	}
-	
-	public void addBet(String risk, String ammount,String user) {
+
+	public void addBet(String risk, double amount, String user) {
 		BettingRequestsImpl userRequest = new BettingRequestsImpl();
-		Bet newBet =new Bet(risk,ammount,user);
+		Bet newBet = new Bet(risk, amount, user);
 		userRequest.createBet(newBet);
 	}
-	
-	public String getAllBets(String user){
-		List<Bet> bets = getAllBetsOfUser(user);		
+
+	public String getAllBets(String user) {
+		List<Bet> bets = getAllBetsOfUser(user);
 		StringBuilder builder = new StringBuilder();
 		builder.append("<ul>");
 		for (Bet b : bets) {
@@ -44,16 +38,22 @@ public class BettingImp implements Betting {
 			builder.append(", ");
 			builder.append(b.getAmmount());
 			builder.append("</li>");
-
 		}
 		builder.append("</ul>");
 		return builder.toString();
 	}
-	
-	private List<Bet> getAllBetsOfUser(String user){
+
+	private List<Bet> getAllBetsOfUser(String user) {
 		BettingRequestsImpl betRequest = new BettingRequestsImpl();
 		Bet tempbet = new Bet();
 		tempbet.setUser(user);
 		return betRequest.findBet(tempbet);
+	}
+
+	private List<User> getUser(String user) {
+		UserRequestImpl req = new UserRequestImpl();
+		User tempusr = new User();
+		tempusr.setUsername(user);
+		return req.getUser(tempusr);
 	}
 }
