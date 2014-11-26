c2d74aeada00c7049fdf21f203ce4c0e90033f36
borgmatthew <borgmb@gmail.com>
2014-11-26 20:29:36 +0100

    added function to post data on form submit and modify current bets accordingly

--- src/main/webapp/javascript/bettingPage.js
+++ src/main/webapp/javascript/bettingPage.js
@@ -1,18 +1,31 @@
-/**
- * 
- */
-var AMMOUNT_ERROR_STRING=	"Invalid Ammount";
+var AMMOUNT_ERROR_STRING="Invalid Ammount";
 var AMMOUNT_APPROVED_STRING="";
 
 $(document).ready(function(){
 	$('#ammount').blur(function(){
 		validateAmmount($('#ammount').val());
 	});
-})
+	
+	$('#betting_form').submit(function(event){
+		event.preventDefault();
+		var amount = $('#ammount').val();
+		var risk = $("input[name=betrisk]:checked").val();
+		if(validateAmmount(amount)){
+			$.post("successfullBetting.jsp", {amm: amount, betrisk: risk}, function(data){
+				var parse = JSON.parse(data);
+				$('#Bett_error').html(parse.message);
+				if(parse.success == "true"){
+					$('#past_bets > ul').append("<li>" + risk + ", " + amount + "</li>");
+				}
+				$('#ammount').val("");
+			});
+		}
+	});
+});
 
 function validateAmmount(amm){
 	var result= false;
-	var regx=/[^0-9\s]/g;
+	var regx=/[^0-9\.]/g;
 	if(amm.match(regx)){
 		$('#ammount_error').html(AMMOUNT_ERROR_STRING);
 		$('#ammount').removeClass("good");
