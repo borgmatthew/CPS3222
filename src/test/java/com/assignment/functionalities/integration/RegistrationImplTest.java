02db055b2aebbf9f5d351507c01ac050856bdb1c
borgmatthew <borgmb@gmail.com>
2014-12-01 22:20:53 +0100

    Fixed registration test for username, inserted username in database before test executes

---
+++ src/test/java/com/assignment/functionalities/integration/RegistrationImplTest.java
@@ -0,0 +1,59 @@
+package com.assignment.functionalities.integration;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.assignment.DBObjects.User;
+import com.assignment.functionalities.Registration;
+import com.assignment.functionalities.RegistrationImpl;
+import com.assignment.requests.UserRequest;
+import com.assignment.requests.UserRequestImpl;
+import com.assignment.util.Props;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+
+public class RegistrationImplTest {
+	
+	private Registration reg;
+	private MongoClient client;
+	private String dbname = Props.getProperty("user_db");
+	private String tblname = Props.getProperty("user_tbl");
+
+	@Before
+	public void setUp() throws Exception {
+		reg = new RegistrationImpl();
+		client = new MongoClient(Props.getProperty("host"),
+				Integer.parseInt(Props.getProperty("port")));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		client.getDB(dbname).getCollection(tblname).remove(new BasicDBObject());
+		client.close();
+	}
+
+	@Test
+	public void TestingValidateFormWithIncorrectUserNameFieldTest() {
+		//given
+		UserRequest request = new UserRequestImpl();
+		User user = new User("", "","ali.speed6@gmail.com", "","", "","", "","", 0,0);
+		request.createUser(user);
+		String name="Alastair";
+		String sname="Vella";
+		String username="ali.speed6@gmail.com";
+		String password="Assignmentpass";
+		String dob="11/12/1991";
+		String type="free";
+		String card="378282246310005";
+		String exp="11/12/2017";
+		String cvv="123";
+		//when
+		boolean result = reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv);
+		//then
+		assertFalse(result);
+	}
+
+}
--- src/test/java/com/assignment/validations/RegistrationTests.java
+++ src/test/java/com/assignment/validations/RegistrationTests.java
@@ -1,6 +1,8 @@
 package com.assignment.validations;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,10 +12,11 @@ import com.assignment.functionalities.Registration;
 import com.assignment.functionalities.RegistrationImpl;
 
 public class RegistrationTests {
-	Registration reg;
+	private Registration reg;
+
 	@Before
 	public void setUp() throws Exception {
-		reg=new RegistrationImpl();
+		reg = new RegistrationImpl();
 	}
 
 	@After
@@ -22,136 +25,138 @@ public class RegistrationTests {
 
 	@Test
 	public void CorrectFieldsTest() {
-		String name="Alastair";
-		String sname="Vella";
-		String username="alx.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		assertTrue(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alastair";
+		String sname = "Vella";
+		String username = "alx.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2017";
+		String cvv = "123";
+		assertTrue(reg.validateForm(name, sname, username, password, dob, type,
+				card, exp, cvv));
 	}
-	
+
 	@Test
 	public void TestingValidateFormWithIncorrectNameFieldTest() {
-		String name="Alast5air";
-		String sname="Vella";
-		String username="alx.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alast5air";
+		String sname = "Vella";
+		String username = "alx.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2017";
+		String cvv = "123";
+		assertFalse(reg.validateForm(name, sname, username, password, dob,
+				type, card, exp, cvv));
 	}
+
 	@Test
 	public void TestingValidateFormWithIncorrectSNameFieldTest() {
-		String name="Alastair";
-		String sname="Vel4la";
-		String username="alx.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alastair";
+		String sname = "Vel4la";
+		String username = "alx.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2017";
+		String cvv = "123";
+		assertFalse(reg.validateForm(name, sname, username, password, dob,
+				type, card, exp, cvv));
 	}
-	@Test
-	public void TestingValidateFormWithIncorrectUserNameFieldTest() {
-		String name="Alastair";
-		String sname="Vella";
-		String username="ali.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
-	}
+
 	@Test
 	public void TestingValidateFormWithIncorrectPasswordFieldTest() {
-		String name="Alastair";
-		String sname="Vella";
-		String username="alx.speed6@gmail.com";
-		String password="As";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alastair";
+		String sname = "Vella";
+		String username = "alx.speed6@gmail.com";
+		String password = "As";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2017";
+		String cvv = "123";
+		assertFalse(reg.validateForm(name, sname, username, password, dob,
+				type, card, exp, cvv));
 	}
+
 	@Test
 	public void TestingValidateFormWithIncorrectDobFieldTest() {
-		String name="Alastair";
-		String sname="Vella";
-		String username="alr.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1999";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alastair";
+		String sname = "Vella";
+		String username = "alr.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1999";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2017";
+		String cvv = "123";
+		assertFalse(reg.validateForm(name, sname, username, password, dob,
+				type, card, exp, cvv));
 	}
+
 	@Test
 	public void TestingValidateFormWithIncorrectCardFieldTest() {
-		String name="Alastair";
-		String sname="Vella";
-		String username="alx.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="308282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alastair";
+		String sname = "Vella";
+		String username = "alx.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "308282246310005";
+		String exp = "11/12/2017";
+		String cvv = "123";
+		assertFalse(reg.validateForm(name, sname, username, password, dob,
+				type, card, exp, cvv));
 	}
+
 	@Test
 	public void TestingValidateFormWithIncorrectExpiryFieldTest() {
-		String name="Alastair";
-		String sname="Vella";
-		String username="alx.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2013";
-		String cvv="123";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alastair";
+		String sname = "Vella";
+		String username = "alx.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2013";
+		String cvv = "123";
+		assertFalse(reg.validateForm(name, sname, username, password, dob,
+				type, card, exp, cvv));
 	}
+
 	@Test
 	public void TestingValidateFormWithIncorrectCvvFieldTest() {
-		String name="Alastair";
-		String sname="Vella";
-		String username="alx.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="12";
-		assertFalse(reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv));
+		String name = "Alastair";
+		String sname = "Vella";
+		String username = "alx.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2017";
+		String cvv = "12";
+		assertFalse(reg.validateForm(name, sname, username, password, dob,
+				type, card, exp, cvv));
 	}
-	
+
 	@Test
 	public void getMessageTest() {
-		String name="Alas5air";
-		String sname="Vella";
-		String username="alx.speed6@gmail.com";
-		String password="Assignmentpass";
-		String dob="11/12/1991";
-		String type="free";
-		String card="378282246310005";
-		String exp="11/12/2017";
-		String cvv="123";
-		reg.validateForm(name, sname, username, password, dob, type, card, exp, cvv);
-		assertEquals("Invalid name",reg.getMessage());
+		String name = "Alas5air";
+		String sname = "Vella";
+		String username = "alx.speed6@gmail.com";
+		String password = "Assignmentpass";
+		String dob = "11/12/1991";
+		String type = "free";
+		String card = "378282246310005";
+		String exp = "11/12/2017";
+		String cvv = "123";
+		reg.validateForm(name, sname, username, password, dob, type, card, exp,
+				cvv);
+		assertEquals("Invalid name", reg.getMessage());
 	}
 
 }
