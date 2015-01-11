5ae07ce068c5edf9a4dbccdb6dc5bf10a1d3158a
borgmatthew <borgmb@gmail.com>
2015-01-11 15:05:23 +0100

    Report

--- src/test/java/com/assignment/model/TestLauncher.java
+++ src/test/java/com/assignment/model/TestLauncher.java
@@ -16,7 +16,7 @@ import com.mongodb.MongoClient;
 
 public class TestLauncher {
 	
-	private final int USERS = 2;
+	private final int USERS = 100;
 	private AtomicBoolean allThreadsStarted = new AtomicBoolean(false);
 	private AtomicInteger browsersOpened = new AtomicInteger(0);
 	
